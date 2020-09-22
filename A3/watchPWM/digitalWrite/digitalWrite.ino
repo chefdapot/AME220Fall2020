b70d7c22c33de119ae31aff390a2f47e3a5cc8a9
Chef McKenzie <chef@ChefBook-Pro.local>
2020-09-22 05:29:22 -0700

    Adding A3 folder

---
+++ A3/watchPWM/digitalWrite/digitalWrite.ino
@@ -0,0 +1,16 @@
+#define LILYGO_WATCH_2019_WITH_TOUCH
+#include <LilyGoWatch.h>
+
+void setup() {
+  pinMode(22, OUTPUT);
+  pinMode(21, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(22, HIGH);
+  digitalWrite(21, HIGH);
+  delay(1800);
+  digitalWrite(22, LOW);
+  digitalWrite(21, LOW);
+  delay(1200);
+}
